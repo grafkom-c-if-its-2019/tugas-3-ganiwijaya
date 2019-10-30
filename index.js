3a03e8b80e40ef9533c938792b6b1408371eae59
Gani <ganiwijaya@live.com>
2019-10-30 19:37:20 +0700

    memindahkan objek

--- index.js
+++ index.js
@@ -44,62 +44,7 @@
     //   +0.5, -0.5,  0.0, 0.25,  +0.5, 0.0
     // ]);
 
-    var panjang = new Float32Array([
-      0.5, 0.06,
-      0.95, 0.06,
-      0.95, -0.08,
-      0.5, -0.08,
-    ]);
-
-    var vertices = new Float32Array(1244);
-
-    for (var i=90; i<=400; i+=1) {
-      // degrees to radians
-      var j = i * Math.PI / 180;
-      // X Y Z
-        vertices[(i - 90) * 4] = Math.sin(j)*0.4 + 0.5;
-        vertices[(i - 90) * 4 + 1] = Math.cos(j)*0.4;
-        // 0,
-      //var vert2 = [
-      //  0,
-      //  0,
-      //  0,
-      // ];
-      DONUT:
-        vertices[(i - 90) * 4 + 2] = Math.sin(j)*0.24 + 0.5;
-        vertices[(i - 90) * 4 + 3] = Math.cos(j)*0.24;
-    }
-
-    var lengkung = new Float32Array(1224);
-
-    for (var i=97; i<=400; i+=1) {
-      // degrees to radians
-      var j = i * Math.PI / 180;
-      // X Y Z
-        lengkung[(i - 97) * 2] = Math.sin(j)*0.4 - 0.5;
-        lengkung[(i - 97) * 2 + 1] = Math.cos(j)*0.4;
-    }
-
-    for (var i=400; i>=97; i-=1) {
-      // degrees to radians
-      var j = i * Math.PI / 180;
-      // X Y Z
-        lengkung[(704 - i) * 2] = Math.sin(j)*0.24 - 0.5;
-        lengkung[(704 - i) * 2 + 1] = Math.cos(j)*0.24;
-    }
-
-    lengkung[1216] = -0.5; lengkung[1217] = -0.08;
-    lengkung[1218] = -0.5; lengkung[1219] = 0.06;
-    lengkung[1220] = -0.05; lengkung[1221] = 0.06;
-    lengkung[1222] = -0.05; lengkung[1223] = -0.08;
-
-    drawA(gl.LINE_LOOP, lengkung);
-    drawA(gl.TRIANGLE_STRIP, vertices);
-    drawA(gl.TRIANGLE_FAN, panjang);
-
-    // drawA(gl.POINTS, pointsVertices);
-    // drawA(gl.LINES, linesVertices);
-    // drawA(gl.TRIANGLES, triangleVertices);
+    
   }
 
   function drawPoint() {
