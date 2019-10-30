4409814c68aa99728c97a593c5e3f8f24ea71fb6
Gani <ganiwijaya@live.com>
2019-10-30 22:11:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-(function() {
-
+(function() 
+{
   glUtils.SL.init({ callback: function() { main(); } });
-
-  function main() {
-    
+  function main() 
+  {
     var canvas = document.getElementById("glcanvas");
     var gl = glUtils.checkWebGL(canvas);
-
     var vertexShader = glUtils.getShader(gl, gl.VERTEX_SHADER, glUtils.SL.Shaders.v1.vertex);
     var fragmentShader = glUtils.getShader(gl, gl.FRAGMENT_SHADER, glUtils.SL.Shaders.v1.fragment);
     var program = glUtils.createProgram(gl, vertexShader, fragmentShader);
@@ -20,14 +18,14 @@
       0.5, -0.08,
     ]);
 
-    var vertices = new Float32Array(1244);
-
-    for (var i=90; i<=400; i+=1) {
+    var bulet = new Float32Array(1244);
+    for (var i=90; i<=400; i+=1) 
+    {
       // degrees to radians
       var j = i * Math.PI / 180;
       // X Y Z
-        vertices[(i - 90) * 4] = Math.sin(j)*0.4 + 0.5;
-        vertices[(i - 90) * 4 + 1] = Math.cos(j)*0.4;
+        bulet[(i - 90) * 4] = Math.sin(j)*0.4 + 0.5;
+        bulet[(i - 90) * 4 + 1] = Math.cos(j)*0.4;
         // 0,
       //var vert2 = [
       //  0,
@@ -35,14 +33,13 @@
       //  0,
       // ];
       DONUT:
-        vertices[(i - 90) * 4 + 2] = Math.sin(j)*0.24 + 0.5;
-        vertices[(i - 90) * 4 + 3] = Math.cos(j)*0.24;
+        bulet[(i - 90) * 4 + 2] = Math.sin(j)*0.24 + 0.5;
+        bulet[(i - 90) * 4 + 3] = Math.cos(j)*0.24;
     }
 
-    // Link antara CPU Memory dengan GPU Memory
     var triangleVertexBufferObject = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, panjang, gl.STATIC_DRAW);
 
     // Link untuk attribute
     var vPosition = gl.getAttribLocation(program, 'vPosition');
@@ -54,84 +51,84 @@
       0,			// ukuran byte tiap vertex
       0				// offset dari posisi elemen di array
     );
-    gl.enableVertexAttribArray(vPosition);
 
-	var translation = gl.getUniformLocation(program, 'translation');
+    gl.enableVertexAttribArray(vPosition);
+    var translation = gl.getUniformLocation(program, 'translation');
     var translationVector = [0.0, 0.0, 0.0];
     gl.uniform3fv(translation, translationVector);
-
     var thetaLocation = gl.getUniformLocation(program, 'theta');
     var theta = 0.0;
-
     var scaleXLocation = gl.getUniformLocation(program, 'scaleX');
     var scaleYLocation = gl.getUniformLocation(program, 'scaleY');
     var scaleX = 1.0;
     var scaleY = 1.0;
-    var melebar = 1;
-	
-	var theta_temp;
-	var scaleX_temp;
-	var scaleY_temp;
+    var stretch = 1;
+    var theta_temp;
+    var scaleX_temp;
+    var scaleY_temp;
 
-    function render() {
+    function render() 
+    {
       // Bersihkan layar jadi hitam
       gl.clearColor(0.56, 0.67, 0.86, 1.0);
-  
       // Bersihkan buffernya canvas
       gl.clear(gl.COLOR_BUFFER_BIT);
 
-	  scaleX_temp = scaleX;
-	  theta_temp = theta;
+      scaleX_temp = scaleX;
+      theta_temp = theta;
 
       theta += 0.0101;
 	 
-      if (scaleX >= 1) melebar = -1;
-      else if (scaleX <= -1) melebar = 1;
-      scaleX += 0.0101 * melebar;
-
-  drawA(gl.TRIANGLE_STRIP, vertices);
-  drawA(gl.TRIANGLE_FAN, panjang);
-    requestAnimationFrame(render);
+      if (scaleX >= 1) stretch = -1;
+      else if (scaleX <= -1) stretch = 1;
+      scaleX += 0.0101 * stretch;
+      drawA(gl.TRIANGLE_STRIP, bulet, 2);
+      drawA(gl.TRIANGLE_FAN, panjang, 2);
+      requestAnimationFrame(render);
     }
-	render();
-	
-	function drawA(type, vertices, cek) {
-    var n = vertices.length / 2;
-	
-	if (cek == 2) {
-		gl.uniform1f(thetaLocation, 0.0);
-		gl.uniform1f(scaleXLocation, scaleX_temp);
-		gl.uniform1f(scaleYLocation, scaleY);
-	}
-	else {
-		gl.uniform1f(thetaLocation, theta_temp);
-		gl.uniform1f(scaleXLocation, 1.0);
-		gl.uniform1f(scaleYLocation, scaleY);
-	}
-	
-	var vertexBuffer = gl.createBuffer();
-    if (!vertexBuffer) {
-      console.log('Failed to create the buffer object');
-      return -1;
-    }
+    render();
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    function drawA(type, vertices, cek) 
+    {
+      var n = vertices.length / 2;
+      if (cek == 2) 
+      {
+        gl.uniform1f(thetaLocation, 0.0);
+        gl.uniform1f(scaleXLocation, scaleX_temp);
+        gl.uniform1f(scaleYLocation, scaleY);
+      }
+      else
+      {
+        gl.uniform1f(thetaLocation, theta_temp);
+        gl.uniform1f(scaleXLocation, 1.0);
+        gl.uniform1f(scaleYLocation, scaleY);
+      }
 
-    var aPosition = gl.getAttribLocation(program, 'aPosition');
-    if (aPosition < 0) {
-      console.log('Failed to get the storage location of aPosition');
-      return -1;
-    }
+      var vertexBuffer = gl.createBuffer();
+      if (!vertexBuffer) 
+      {
+        console.log('Failed to create the buffer object');
+        return -1;
+      }
 
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(aPosition);
-    if (n < 0) {
-      console.log('Failed to set the positions of the vertices');
-      return;
+      gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+      gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+      var aPosition = gl.getAttribLocation(program, 'aPosition');
+      if (aPosition < 0) 
+      {
+        console.log('Failed to get the storage location of aPosition');
+        return -1;
+      }
+
+      gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+      gl.enableVertexAttribArray(aPosition);
+      if (n < 0) 
+      {
+        console.log('Failed to set the positions of the vertices');
+        return;
+      }
+      gl.drawArrays(type, 0, n);
     }
-    gl.drawArrays(type, 0, n);
   }
-  }
-}
-)();
+})();
